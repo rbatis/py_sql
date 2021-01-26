d006f102cbcbf2090745abe6357f43c99a2ab6df
zxj_pc <zhuxiujia@qq.com>
2021-01-26 23:58:19 +0800

    fmt code

--- tests/bench_test.rs
+++ tests/bench_test.rs
@@ -16,12 +16,25 @@ pub fn test_bench_py_sqsl_select() {
     let py_runtime = PyRuntime::new(vec![]);
     let engine = RExprRuntime::new();
     let (sql,arg) = py_runtime.eval(&DriverType{}, "select * from table where
-                                                                                                if 1 == 1:
-                                                                                                   column = 1
-                                                                                                and age in (1,2,3)
-                                                                                                if 2==2:
-                                                                                                and age = 2",
-                                    &mut serde_json::json!({}), &engine).unwrap();
+                                                                  id = #{id}
+                                                                  id != #{id}
+                                                                  id in #{ids}
+                                                                  id in #{ids}
+                                                                  id in #{ids}
+                                                                  id not in #{ids}
+                                                                  for k,v in map:
+                                                                     #{k}=#{v}
+                                                                  name like #{name}
+                                                                  or
+                                                                  name not like #{name}
+                                                                  create_time between #{create_time} and #{create_time}
+                                                                  group by
+                                                                  for item in ids:
+                                                                     #{item}
+                                                                  order by
+                                                                  for item in order_by:
+                                                                     #{item}",
+                                    &mut serde_json::json!({"id":1,"order_by":["id","name"],"ids":[1,2,3],"name":"asdf","map":{"a":1},"create_time":"2020-23-23"}), &engine).unwrap();
     println!("sql:{},arg:{:?}", sql, arg);
     let total = 10000;
     let now = std::time::Instant::now();
